5a104c55238d90ceca722579c43c0307ff8adc3c
liji <lijiredback@163.com>
2020-08-02 09:47:27 +0800

    feat: week15

--- week15/animation/animation.js
+++ week15/animation/animation.js
@@ -17,16 +17,16 @@ export class Timeline {
 
         for (let animation of animations) {
 
-            let { object, property, template, start, end, duration, timingFunction, delay } = animation
+            let { object, property, template, start, end, duration, timingFunction, delay, addTime } = animation
 
-            let progression = timingFunction((t - delay) / duration) // 0 - 1 之间的数
+            let progression = timingFunction((t - delay - addTime) / duration) // 0 - 1 之间的数
 
-            if (t > animation.duration + animation.delay) {
+            if (t > duration + delay + addTime) {
                 progression = 1
                 animation.finished = true
             }
 
-            let value = start + progression * (end - start) // value 是根据 progression 算出的
+            let value = animation.valueFromProgression(progression) // value 是根据 progression 算出的
 
             object[property] = template(value)
         }
@@ -73,13 +73,18 @@ export class Timeline {
         this.tick()
     }
 
-    add(animation) {
+    add(animation, addTime) {
         this.animations.push(animation)
+        animation.finished = false
+        if (this.state === 'playing')
+            animation.addTime = addTime !== void 0 ? addTime : Date.now() - this.startTime
+        else
+            animation.addTime = addTime !== void 0 ? addTime : 0
     }
 }
 
 export class Animation {
-    constructor(object, property, template, start, end, duration, delay, timingFunction) {
+    constructor(object, property, start, end, duration, delay, timingFunction, template) {
         this.object = object
         this.property = property
         this.template = template
@@ -88,6 +93,32 @@ export class Animation {
         this.duration = duration
         this.delay = delay
         this.timingFunction = timingFunction
+    }
+
+    valueFromProgression(progression) {
+        return this.start + progression * (this.end - this.start)
+    }
+}
+
+export class ColorAnimation {
+    constructor(object, property, start, end, duration, delay, timingFunction, template) {
+        this.object = object
+        this.property = property
+        this.template = template || (v => `rgba(${v.r}, ${v.g}, ${v.b}, ${v.a})`)
+        this.start = start
+        this.end = end
+        this.duration = duration
+        this.delay = delay
+        this.timingFunction = timingFunction
+    }
+
+    valueFromProgression(progression) {
+        return {
+            r: this.start.r + progression * (this.end.r - this.start.r),
+            g: this.start.g + progression * (this.end.g - this.start.g),
+            b: this.start.b + progression * (this.end.b - this.start.b),
+            a: this.start.a + progression * (this.end.a - this.start.a),
+        }
     }
 }
 
